2e041c470506738968debebd5a3c4d5b2954ffff
Lucas Moraes <lucasmoraes86@hotmail.com>
2023-06-29 19:53:29 -0300

    run app

---
+++ src/main/java/com/example/tcs3semestre/Tcs3semestreApplication.java
@@ -0,0 +1,13 @@
+package com.example.tcs3semestre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tcs3semestreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tcs3semestreApplication.class, args);
+	}
+
+}
